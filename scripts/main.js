652ab2dbdde0f8bebad7e4c30ad7890c84fbb884
Aryan Sharma <aryansharma24106@gmail.com>
2025-09-11 04:28:34 +0530

    add final html and css

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".menu-grid");
-
-  container.addEventListener("wheel", function (e) {
-    // only hijack vertical scrolling
-    if (e.deltaY !== 0) {
-      container.scrollLeft += e.deltaY;  // scroll horizontally instead
-      e.preventDefault();                // stop page vertical scroll
-    }
-  }, { passive: false });
+document.querySelector(".explore-menu").addEventListener("click", () => {
+  document.getElementById("menu-section").scrollIntoView({
+    behavior: "smooth"
+  });
 });
